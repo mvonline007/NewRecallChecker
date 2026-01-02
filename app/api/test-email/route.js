25a6a453d1592ead7bae10997509d58f85de8e47
mvonline007 <marc.verzotti@gmail.com>
2026-01-02 11:37:34 +0100

    Simplify email motif display

--- app/api/test-email/route.js
+++ app/api/test-email/route.js
@@ -4,7 +4,7 @@ import { buildEmailHtml } from "@/lib/email-template";
 import { fetchRssItems, VERSION as RSS_VERSION } from "@/lib/rss";
 
 export const runtime = "nodejs";
-export const VERSION = "1.0.31";
+export const VERSION = "1.0.32";
 
 const CRON_SECRET = process.env.CRON_SECRET;
 
